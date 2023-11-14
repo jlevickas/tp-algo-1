e5ae106692448a6b3c506431e76b75181fdf10df
tomas modafferi <tomasmodafferi@gmail.com>
2023-11-14 12:54:50 -0300

    listado

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # tp-algo1
-TP Parcial algoritmos 1
+TP Parcial algoritmos 1 2023
--- biblioteca.h
+++ biblioteca.h
@@ -34,7 +34,22 @@ boolean existeLibro(Biblioteca b, Libro l){
     }
 }
 
+char * buscarLibro(Biblioteca b, char* libroDeseado) {
+    primero(&b);
+    while (!esUltimo(b)){
+        Libro libroActual = getDato(b);
+        char* tituloActual = get_titulo(libroActual);
 
+        if (strcmp(tituloActual, libroDeseado) == 0) {
+            return (toString(libroActual));
+        }
+        else{
+        siguiente(&b);
+        }
+    }
+}
+
+//primero
 void listarAutores(Biblioteca b){
     primero(&b);
     while(!esUltimo(b)){
@@ -43,10 +58,75 @@ void listarAutores(Biblioteca b){
     }
 }
 
+//segundo
 void listarTitulos(Biblioteca b){
     primero(&b);
     while(!esUltimo(b)){
         printf("%s\n", get_titulo(getDato(b)));
+        siguiente(&b);
+    }
+}
+
+//tercero
+void listarGenero(Biblioteca b, char* generoDeseado) {
+    primero(&b);
+    while (!esUltimo(b)){
+        Libro libroActual = getDato(b);
+        char* generoActual = get_genero(libroActual);
+
+        if (strcmp(generoActual, generoDeseado) == 0) {
+            printf("Libro : %s\n", toString(libroActual));
+            printf("\n");
+        }
+
+        siguiente(&b);
+    }
+}
+
+//cuarto
+void listarAutor(Biblioteca b, char* autorDeseado) {
+    primero(&b);
+    while (!esUltimo(b)){
+        Libro libroActual = getDato(b);
+        char* autorActual = get_autores(libroActual);
+
+        if (strcmp(autorActual, autorDeseado) == 0) {
+            printf("Libro : %s\n", toString(libroActual));
+            printf("\n");
+        }
+
+        siguiente(&b);
+    }
+}
+
+//quinto
+void listarEditorial(Biblioteca b, char* editorialDeseada) {
+    primero(&b);
+    while (!esUltimo(b)){
+        Libro libroActual = getDato(b);
+        char* editorialActual = get_editorial(libroActual);
+
+        if (strcmp(editorialActual, editorialDeseada) == 0) {
+            printf("Libro : %s\n", toString(libroActual));
+            printf("\n");
+        }
+
+        siguiente(&b);
+    }
+}
+
+void listarEditorialPorEdicion(Biblioteca b, char* editorialDeseada, int anio1, int anio2) {
+    primero(&b);
+    while (!esUltimo(b)){
+        Libro libroActual = getDato(b);
+        char* editorialActual = get_editorial(libroActual);
+
+        if (strcmp(editorialActual, editorialDeseada) == 0) {
+            if(get_anio_edicion(libroActual)<=anio2 && get_anio_edicion(libroActual)>=anio1){
+                printf("Libro : %s\n", toString(libroActual));
+                printf("\n");
+            }
+        }
         siguiente(&b);
     }
 }
